46455c96ad3ef566ff7aa6cb71b16a68402eecf5
Kylee Webb <kawebb@sdsu.edu>
2019-09-29 16:26:41 -0700

    About to try to add external css file

--- scripts/map.js
+++ scripts/map.js
@@ -1,11 +1,6 @@
 define([
-	"esri/Map",
-	"esri/dijit/Popup", "esri/dijit/PopupTemplate",
-	"esri/layers/FeatureLayer",
-	"esri/symbols/SimpleFillSymbol", "esri/Color",
-	"dojo/dom-class", "dojo/dom-construct", "dojo/on",
-	"dojox/charting/Chart", "dojox/charting/themes/Dollar",
-	"dojo/domReady!"
+	"esri/Map"
+	
 ],
 function(
 	Map
