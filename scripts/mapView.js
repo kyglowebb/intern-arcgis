c6d9dcd5e9d617deb3fe57851c20857d89826794
Kylee Webb <kawebb@sdsu.edu>
2019-09-30 14:21:15 -0700

    Update mapView.js
    zoomed in now to US

--- scripts/mapView.js
+++ scripts/mapView.js
@@ -15,7 +15,7 @@ function(
 	var viewProperties = {
 		map: map,
 		container: Constants.MAP_VIEW_DIV_ID,
-		// zoom: 4,
+		zoom: 4,
 		center: [-95.7129, 37.0902]
 	}
 	var view = new MapView(viewProperties);
