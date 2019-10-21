e382eb5fc09b84e3afdc5d6b2e2d9913ab2bf725
Kylee Webb <kawebb@sdsu.edu>
2019-10-20 17:55:10 -0700

    Connecting CSV to Feature Layer
    Now I am trying to connect the CSV layer to my popup template which is put in as a Feature Layer. This way I can put the URL hopefully as my CSV file since I am not sure how to make my CSV file a website url.

--- scripts/init.js
+++ scripts/init.js
@@ -4,14 +4,30 @@ require([
 	"esri/PopupTemplate",
 	"esri/arcadeProfiles/popupProfile",
 	"esri/arcgis/utils",
-	"dojo/domReady!"
+	"dojo/domReady!",
+	// added these to put in feature layer because I guess this is where the layers go?
+	"esri/layers/FeatureLayer"
 ],
-function(
+function(                     // my guess is init is where you put your main functions?
 	mapView,
 	PopupTemplate,
     popupProfile,
-    arcgisUtils
+	arcgisUtils,
+	FeatureLayer               // Create a featureLayer & add a rest endpoint of the restaurants
 ){
 
+	var restaurant = new FeatureLayer ({
+		url:"https://services3.arcgis.com/GVgbJbqm8hXASVYi/ArcGIS/rest/services/Routes_to_Restaurants_(Points)/FeatureServer/0"
+	});
+	map.add(restaurant,0);
 	
+	var popupRestaurants = {
+		"title": "{TITLE}",
+		"content": "<b>City:</b> {city}<br> <b>State:</b> {state}<br> <b>Name:</b> {name}<br> <b>Longitutde:</b> {longitude}<br> <b>Latitude:</b> {latitude}<br> <b>Phone Number:</b> {phone}<br>"
+	}
+
+	var displayRestaurants = new FeatureLayer ({ 
+		url: ""
+	})
+
 });
