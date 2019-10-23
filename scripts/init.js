81e324310e3e3fb5f3806aadc45107fb9458f7e6
Kylee Webb <kawebb@sdsu.edu>
2019-10-22 20:13:15 -0700

    Trying to get popup template to render correctly!

--- scripts/init.js
+++ scripts/init.js
@@ -1,50 +1,52 @@
 require([
-	"scripts/mapView", 	// import mapView
-	// added these below for popup make sure updated csv layer
-	"esri/PopupTemplate",
-	//"esri/arcadeProfiles/popupProfile",
-	//"esri/arcgis/utils",
-	// added these to put in feature layer because I guess this is where the layers go?
+	"scripts/mapView", 	
 	"esri/layers/FeatureLayer",
-	"esri/layers/CSVLayer",  // Perhaps I don't need this because it will be done through Feature Layer?
+	"esri/PopupTemplate",
+	"esri/layers/CSVLayer", 
 	"dojo/domReady!",
 
 ],
-function(                     // my guess is init is where you put your main functions?
+function(                     
 	mapView,
-	PopupTemplate,
-    //popupProfile,
-	//arcgisUtils,
 	FeatureLayer,
-	CSVLayer,               // Create a featureLayer & add a rest endpoint of the restaurants
+	PopupTemplate,
+	CSVLayer,              
 	
 ){
-	// var restaurant = new FeatureLayer ({
-	// 	url:"https://services3.arcgis.com/GVgbJbqm8hXASVYi/ArcGIS/rest/services/Routes_to_Restaurants_(Points)/FeatureServer/0"
-	// });
-	// mapView.map.add(restaurant,0);
 
-	var popupRestaurants = new PopupTemplate({
-		title: "Top Restaurants in",
-		content: "<b>City:</b> {city}<br> <b>State:</b> {state}<br> <b>Name:</b> {name}<br> <b>Latitude:</b> {latitude}<br> <b>Longitude:</b> {longitude}<br> <b>Phone Number:</b> {phone}<br>"
+	// Create a feature layer to attach the popup template to
+	// Create title and content within the popup template
+	var template = new FeatureLayer ({
+		url: "./info.csv",
+		title: "{NAME} in {CITY}",
+		content: "<b>City:</b>{city}<br><b>State:</b>{state}<br><b>Name:</b>{name}<br><b>Latitude:</b>{latitude}<br><b>Longitude:</b>{longitude}<br><b>Phone Number:</b> {phone}<br>"
 	  });
+
+// Maybe it's because feature layer doesn't know how to make a pop up
+// so first I make feature layer
+// then I connect feature layer to pop up
+// then I display popup by doing the weird dot thing
 
 	  // popupTemplate.title = "Marriage in NY, Zip Code: {ZIP}",
 
-	//popupTemplate.content = "<p>As of 2015, <b>{MARRIEDRATE}%</b> of the" +
-	   //" population in this zip code is married.</p>" +
-	   //"<ul><li>{MARRIED_CY} people are married</li>" +
-	   //"<li>{NEVMARR_CY} have never married</li>" +
-	   //"<li>{DIVORCD_CY} are divorced</li><ul>";
-	
-	
-	var displayRestaurants = new CSVLayer ({ 
+	  // popupTemplate.content = "<p>As of 2015, <b>{MARRIEDRATE}%</b> of the" +
+//" population in this zip code is married.</p>" +
+//"<ul><li>{MARRIED_CY} people are married</li>" +
+//"<li>{NEVMARR_CY} have never married</li>" +
+//"<li>{DIVORCD_CY} are divorced</li><ul>";
+
+	  // Set the PopupTemplate on the layer
+	  template.popupTemplate = display;
+	  // template.popupTemplate = display;
+
+	// displays black dots
+	var display = new CSVLayer ({     
 		url: "./info.csv",
 		outfields: ["city", "state", "name", "latitude", "longitude", "phone"],
 		renderer: {
-			type: "simple",  // autocasts as new SimpleRenderer()
+			type: "simple",  
 			symbol: {
-			  type: "simple-marker",  // autocasts as new SimpleMarkerSymbol()
+			  type: "simple-marker",  
 			  size: 6,
 			  color: "black",
 			  outline: {  // autocasts as new SimpleLineSymbol()
@@ -54,8 +56,16 @@ function(                     // my guess is init is where you put your main fun
 			}
 		  }
 	});
-	mapView.map.add(displayRestaurants);
+	mapView.map.add(display);
+	mapView.map.add(template);
 
-	popupTemplate = popupRestaurants;
+	// I think I need to connect the CSVLayer to the popup template
+	// connect popup to csvLayer
+	// CSVLayer.template;
+
+	//csvLayer.labelingInfo = [ statesLabelClass ];
+
+	// mapView.map.add(template);
+	// mapView.map.add(restaurant,0);
 
 });
