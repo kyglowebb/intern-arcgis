8e406aac0cc9f69d177332672b95e0ced89e1554
Kylee Webb <kawebb@sdsu.edu>
2019-10-22 17:36:16 -0700

    Trying to integrate the popup layer with the feature layer to display the information in my csv files

--- scripts/init.js
+++ scripts/init.js
@@ -1,7 +1,7 @@
 require([
 	"scripts/mapView", 	// import mapView
 	// added these below for popup make sure updated csv layer
-	//"esri/PopupTemplate",
+	"esri/PopupTemplate",
 	//"esri/arcadeProfiles/popupProfile",
 	//"esri/arcgis/utils",
 	// added these to put in feature layer because I guess this is where the layers go?
@@ -12,24 +12,32 @@ require([
 ],
 function(                     // my guess is init is where you put your main functions?
 	mapView,
-	//PopupTemplate,
+	PopupTemplate,
     //popupProfile,
 	//arcgisUtils,
 	FeatureLayer,
 	CSVLayer,               // Create a featureLayer & add a rest endpoint of the restaurants
 	
 ){
-
 	// var restaurant = new FeatureLayer ({
 	// 	url:"https://services3.arcgis.com/GVgbJbqm8hXASVYi/ArcGIS/rest/services/Routes_to_Restaurants_(Points)/FeatureServer/0"
 	// });
 	// mapView.map.add(restaurant,0);
-	
-	var popupRestaurants = {
-		"title": "{TITLE}",
-		"content": "<b>City:</b> {city}<br> <b>State:</b> {state}<br> <b>Name:</b> {name}<br> <b>Longitutde:</b> {longitude}<br> <b>Latitude:</b> {latitude}<br> <b>Phone Number:</b> {phone}<br>"
-	}
 
+	var popupRestaurants = new PopupTemplate({
+		title: "Top Restaurants in",
+		content: "<b>City:</b> {city}<br> <b>State:</b> {state}<br> <b>Name:</b> {name}<br> <b>Latitude:</b> {latitude}<br> <b>Longitude:</b> {longitude}<br> <b>Phone Number:</b> {phone}<br>"
+	  });
+
+	  // popupTemplate.title = "Marriage in NY, Zip Code: {ZIP}",
+
+	//popupTemplate.content = "<p>As of 2015, <b>{MARRIEDRATE}%</b> of the" +
+	   //" population in this zip code is married.</p>" +
+	   //"<ul><li>{MARRIED_CY} people are married</li>" +
+	   //"<li>{NEVMARR_CY} have never married</li>" +
+	   //"<li>{DIVORCD_CY} are divorced</li><ul>";
+	
+	
 	var displayRestaurants = new CSVLayer ({ 
 		url: "./info.csv",
 		outfields: ["city", "state", "name", "latitude", "longitude", "phone"],
@@ -40,12 +48,14 @@ function(                     // my guess is init is where you put your main fun
 			  size: 6,
 			  color: "black",
 			  outline: {  // autocasts as new SimpleLineSymbol()
-				width: 0.5,
+				width: 1,
 				color: "white"
 			  }
 			}
 		  }
 	});
 	mapView.map.add(displayRestaurants);
+
+	popupTemplate = popupRestaurants;
 
 });
--- scripts/map.js
+++ scripts/map.js
@@ -11,6 +11,7 @@ function(
 	
 	var mapProperties = {
 		basemap : 'topo',
+		//layers: [featureLayer],    // added this for the popup
 	};
 	
 	var map = new Map(mapProperties);
